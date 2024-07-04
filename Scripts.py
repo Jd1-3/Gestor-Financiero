af6f518c9f0de45cc375ae39e5bbc1e0e10108fe
Jd1-3 <139600859+Jd1-3@users.noreply.github.com>
2024-07-03 20:09:19 -0500

    v1-Gestor-Financiero

---
+++ Scripts.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def raiz():
+    
+    return render_template("index.html")
+
+@app.route("/Gastos", methods=["POST","GET"])
+def gastos():
+    
+    return render_template("gastos.html")
+
+@app.route("/Ingresos", methods=["POST","GET"])
+def ingresos():
+    
+    return render_template("ingresos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 5000)
+    
